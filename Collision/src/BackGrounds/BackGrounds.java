821a75f862dc6e4667ba630fdcea9e942962c18b
neolink557 <41975283+neolink557@users.noreply.github.com>
2019-01-29 20:25:59 -0500

    añadido proyecto java

---
+++ Collision/src/BackGrounds/BackGrounds.java
@@ -0,0 +1,29 @@
+package BackGrounds;
+
+
+import java.awt.Graphics;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Braya
+ */
+public class BackGrounds {
+        JPanel MiJPB;
+    public BackGrounds(JPanel miJ) {
+        this.MiJPB=miJ;
+    }
+    public void DrawBack(Graphics g,String Direccion)
+    {
+        ImageIcon MiImagen=new ImageIcon(Direccion);
+        g.drawImage(MiImagen.getImage(), 0, 0, MiJPB);
+    }
+    
+}
---
+++ Collision/src/Characthers/Steve.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Characthers;
+
+import java.awt.Graphics;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Braya
+ */
+public class Steve {
+    int x;
+    int y;
+    int change;
+
+    public void setChange(int change) {
+        this.change = change;
+    }
+
+    public int getChange() {
+        return change;
+    }
+    JPanel MiJP;
+    JPanel MiJP2;
+   public Steve(JPanel miJ){
+        this.x=0;
+        this.y=0;
+        this.MiJP=miJ;
+        this.MiJP2=miJ;
+    }
+    
+    public void DrawChar(Graphics g,int xi , int yi,String Direccion)
+    {
+        
+        this.x=xi;
+        this.y=yi;
+        ImageIcon MiImagen=new ImageIcon(Direccion);
+      
+        g.drawImage(MiImagen.getImage(), xi, yi, MiJP);
+        this.MiJP.update(g);
+    }
+    
+    public int CoordX(){
+        return this.x;
+    }
+    
+    public int CoordY(){
+        return this.y;
+    }
+    
+
+}
